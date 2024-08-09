23a95411fcfa0566231957fb0f2efdadc0d08a2a
Samuel Mothwa <samuel.mothwa@shoprite.co.za>
2024-08-09 23:48:47 +0200

    config for release

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 import config from './secrets';
 
 const configs = {
-  APPLICATION_URL: config['API_URL'],
-  // APPLICATION_URL: config['API_URL_PROD'],
+  // APPLICATION_URL: config['API_URL'],
+  APPLICATION_URL: config['API_URL_PROD'],
 };
 
 export default configs;
